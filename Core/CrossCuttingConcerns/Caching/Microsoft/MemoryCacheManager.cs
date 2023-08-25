70ae774e691559c79e4dfa49718cace17b6b0354
Kübra Bol <kubra1272@icloud.com>
2023-08-25 22:44:36 +0300

    Cache

--- Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Core.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.CrossCuttingConcerns.Caching.Microsoft
 {
-    internal class MemoryCacheManager
+    public class MemoryCacheManager : ICacheManager
     {
+        IMemoryCache _memoryCache;
+        public MemoryCacheManager()
+        {
+                _memoryCache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+        }
+        public void Add(string key, object value, int duration)
+        {
+            _memoryCache.Set(key,value,TimeSpan.FromMinutes(duration));
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);    
+        }
+
+        public object Get(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public bool IsAdd(string key)
+        {
+            return _memoryCache.TryGetValue(key,out _); //our bellekte var mı yok mu sadece bak değer döndürme data verme
+        }
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
