fadaaa51b1d44030e0fae190339e4505e37ba981
Kübra Bol <kubra1272@icloud.com>
2023-08-28 23:08:31 +0300

    CacheAspect

--- Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Caching;
+using Core.IoC;
+using Core.Utilities.İnterceptors;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Aspects.Autofac.Caching
 {
-    internal class CacheRemoveAspect
+    public class CacheRemoveAspect : MethodInterception // data bozulduğu zaman çalışır ne zaman bozulur(data eklenirse silinirse güncellenirse)
     {
+        private string _pattern;
+        private ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(string pattern)
+        {
+            _pattern = pattern;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemoveByPattern(_pattern);
+        }
     }
 }
--- Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Castle.DynamicProxy;
+using Core.Utilities.İnterceptors;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Transactions;
 
 namespace Core.Aspects.Autofac.Transaction
 {
-    internal class TransactionScopeAspect
+    public class TransactionScopeAspect : MethodInterception
     {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
     }
 }
